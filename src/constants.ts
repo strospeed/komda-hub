58cf556b073d74bd22b5001749b88f9ad9cd053c
mar.io <mariodimasputra01@gmail.com>
2026-08-03 16:23:19 +0700

    Create constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,19 @@
+export const DISCORD_INVITE_URL = `https://discord.gg/GwXdWBTapD`;
+export const CHURCH_WEBSITE_URL = 'https://gkjslogohimo.web.id/';
+export const CHURCH_EMAIL = 'gkj.slogohimo.wng@gmail.com';
+export const OWNER_EMAIL = 'admin@gkjslogohimo.web.id'; 
+export const LOGO_URL = "https://scontent.cdninstagram.com/v/t51.82787-19/670185764_18404537299198608_3466022258141293919_n.jpg?stp=dst-jpg_s150x150_tt6&_nc_cat=108&ccb=7-5&_nc_sid=f7ccc5&efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLnd3dy4xMDgwLkMzIn0%3D&_nc_ohc=fT8-QoF7sGAQ7kNvwG0YQl8&_nc_oc=AdriMEhEnYQIPNWxsshVgq4awx68DrA7n_3KkfQFiP0zhIhNCEfLmo2s5-U-E-Ye6cw&_nc_zt=24&_nc_ht=scontent.cdninstagram.com&_nc_gid=stV9ZRyT4yRV4ZTzPFPOrg&_nc_ss=7b6a8&oh=00_AQHN3R0HJWbuIvSDRWDJ2WbmT8UNXJQY__b5tuHSxuvyjw&oe=6A751827";
+export const LIGHT_WALLPAPER = "https://cdn.phototourl.com/free/2026-08-02-a73c9136-d84c-458e-9a24-5dd81573907f.png";
+export const DARK_WALLPAPER = "https://cdn.phototourl.com/free/2026-08-02-1d051970-5df8-430e-bfbc-7a6058ad168c.png";
+export const PERMANENT_DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1532677061397844089/hHMk-YY4pzLD8Z_WUu_hwMETVUTq0klvbgCv-RPVuMapx_jzs5642I61YfG-PnGbMm65';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAqNuViryXML4war1pXTjxm9l6VIqGhB0A',
+  authDomain: 'komda-hub.firebaseapp.com',
+  projectId: 'komda-hub',
+  storageBucket: 'komda-hub.firebasestorage.app',
+  messagingSenderId: '170131110544',
+  appId: '1:170131110544:web:4238f2d853b83deae8f615',
+  measurementId: 'G-Y1WRHPFVFJ',
+};
+export const appId = 'komda-hub-main';
